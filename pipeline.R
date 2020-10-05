a33ce7d9037d3e1600026ab5b62b9b824f6ec3d6
Colby Ford <colbytylerford@gmail.com>
2020-10-05 10:32:14 -0400

    Update pipeline.R

--- pipeline.R
+++ pipeline.R
@@ -3,8 +3,10 @@
 #   install.packages("BiocManager")
 
 # Hisat2: https://bioconductor.org/packages/release/bioc/vignettes/Rhisat2/inst/doc/Rhisat2.html
+# devtools::install_github("fmicompbio/Rhisat2")
 BiocManager::install("Rhisat2")
 # DESeq2: https://bioconductor.org/packages/release/bioc/vignettes/DESeq2/inst/doc/DESeq2.html
+# devtools::install_github("mikelove/DESeq2")
 BiocManager::install("DESeq2")
 
 ## Load in Packages
