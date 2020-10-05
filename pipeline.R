b2ef72905f1e33114af0271a1dbf004119d3a83a
Colby Ford <colbytylerford@gmail.com>
2020-10-05 10:35:58 -0400

    Add package installation script

--- pipeline.R
+++ pipeline.R
@@ -1,19 +1,9 @@
-## Install Packages
-# if (!requireNamespace("BiocManager", quietly = TRUE))
-#   install.packages("BiocManager")
-
-# Hisat2: https://bioconductor.org/packages/release/bioc/vignettes/Rhisat2/inst/doc/Rhisat2.html
-# devtools::install_github("fmicompbio/Rhisat2")
-BiocManager::install("Rhisat2")
-# DESeq2: https://bioconductor.org/packages/release/bioc/vignettes/DESeq2/inst/doc/DESeq2.html
-# devtools::install_github("mikelove/DESeq2")
-BiocManager::install("DESeq2")
-
 ## Load in Packages
 # library(dplyr)
 # library(Rhisat2)
 # library(DESeq2)
 
+# source("0_install_pkgs.R")
 source("1_align_hisat.R")
 source("2_map.R")
 source("3_comparison_deseq2.R")
