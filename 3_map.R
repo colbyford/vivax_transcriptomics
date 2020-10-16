5222441906a51ac115e527b8e8d9bf9aff8117b5
Colby Ford <colbytylerford@gmail.com>
2020-10-16 13:09:46 -0400

    Fix gene_id in mapping output,

--- 3_map.R
+++ 3_map.R
@@ -17,6 +17,13 @@ feature_counts_df <- feature_counts_iter$counts %>%
   # t() %>% 
   as.data.frame()
 
+## Make sure gene_id is included (instead of as row names)
+feature_counts_df$gene_id <- row.names(feature_counts_df)
+row.names(feature_counts_df) <- NULL
+
+## Move gene_id to the first column
+feature_counts_df <- feature_counts_df[, c(ncol(feature_counts_df), 1:(ncol(feature_counts_df)-1))]
+
 
 ## Write out feature courts dataframe
-write.csv(feature_counts_df, "output/feature_counts.csv", row.names = TRUE)
+write.csv(feature_counts_df, "output/feature_counts.csv", row.names = FALSE)
