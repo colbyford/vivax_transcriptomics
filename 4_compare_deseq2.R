89e186eeea3ca07a33dc75b363ac35bdf87714ea
Colby Ford <colbytylerford@gmail.com>
2020-10-16 13:16:51 -0400

    Update 4_compare_deseq2.R

--- 4_compare_deseq2.R
+++ 4_compare_deseq2.R
@@ -1,1 +1,11 @@
 library(DESeq2)
+
+# https://www.bioconductor.org/packages/devel/bioc/vignettes/DESeq2/inst/doc/DESeq2.html
+
+cts <- read.csv("output/feature_counts.csv",
+                row.names="gene_id") %>%
+  as.matrix()
+
+dds <- DESeqDataSetFromMatrix(countData = cts,
+                              colData = coldata,
+                              design = ~ stage)
