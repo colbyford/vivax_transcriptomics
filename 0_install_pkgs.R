cac7021185e0175d031f6a37ecc7e8f1a281a270
Colby Ford <colby.ford@uncc.edu>
2020-10-16 20:53:40 -0400

    Add CRAN mirror setting to install packages script

--- 0_install_pkgs.R
+++ 0_install_pkgs.R
@@ -1,3 +1,9 @@
+## Set CRAN Mirror
+r = getOption("repos")
+r["CRAN"] = "http://cran.us.r-project.org"
+options(repos = r)
+
+
 ## Install Packages
 if (!requireNamespace("BiocManager", quietly = TRUE)){
   install.packages("BiocManager")
