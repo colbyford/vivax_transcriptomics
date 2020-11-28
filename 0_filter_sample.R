1652293985db32cdaca2b6fe4516935415d4b8c6
Colby Ford <colbytylerford@gmail.com>
2020-11-28 10:35:29 -0500

    Update 0_filter_sample.R

--- 0_filter_sample.R
+++ 0_filter_sample.R
@@ -38,6 +38,8 @@ cat("Now reading sample:", samples$SampleID[sample],"\n")
 
 ## Filter out reads from the input R1 (and R2) sample files
 
+
+## Set up locatl parallelism
 library(doParallel)
 no_cores <- detectCores() - 1
 cl <- makeCluster(no_cores)
